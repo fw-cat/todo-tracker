73f5edc40c1e3c8fca743383d258295f852302cf
miyahara <kenmiya.sns@gmail.com>
2024-09-25 09:42:33 +0900

    [feat]SPAアプリ化

--- front/gatsby-config.js
+++ front/gatsby-config.js
@@ -9,5 +9,25 @@ module.exports = {
     title: `Todo Tracker`,
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [],
+  plugins: [
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `GatsbyJS`,
+        short_name: `GatsbyJS`,
+        start_url: `/`,
+        background_color: `#f7f0eb`,
+        theme_color: `#a2466c`,
+        display: `standalone`,
+        icon: "src/images/icon.png",
+        icons: [
+          {
+            src: `src/images/icon.png`,
+            sizes: `512x512`,
+            type: `image/png`,
+          },
+        ],
+      },
+    }
+  ],
 }
