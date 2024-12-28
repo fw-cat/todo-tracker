ca3c31a99fe23f58ab4e87258156252217f572f9
miyahara <kenmiya.sns@gmail.com>
2024-12-28 16:10:14 +0900

    [feat]新規登録機能を追加

--- src/app/Http/Controllers/Api/User/RegisterController.php
+++ src/app/Http/Controllers/Api/User/RegisterController.php
@@ -7,21 +7,32 @@ use App\Http\Requests\Api\User\Register\PreRequest;
 use App\Http\Resources\UserResource;
 use App\Services\User\RegisterService;
 use Illuminate\Auth\AuthenticationException;
+use Illuminate\Auth\AuthManager;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 
 class RegisterController extends Controller
 {
+    /**
+     * @param AuthManager $auth
+     */
+    public function __construct(
+        private AuthManager $auth,
+    ) {
+    }
+
     public function pre(PreRequest $request, RegisterService $service): JsonResponse
     {
         $preUser = $service->pre($request);
         if (!$preUser) {
             throw new AuthenticationException();
         }
+        $this->auth->login($preUser);
+        $request->session()->regenerate();
+
         return new JsonResponse([
             "message" => "Pre Registerd.",
-            'user' => new UserResource($preUser),
         ], Response::HTTP_OK);
     }
 
